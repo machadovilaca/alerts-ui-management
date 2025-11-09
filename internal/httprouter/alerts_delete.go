55d7f3e8b2475d291c7409ad8d6b15058c85e5fc
avlitman <alitman@redhat.com>
2025-11-09 14:38:23 +0200

    Add /api/v1/alerting/rules/{ruleId} DELETE
    Signed-off-by: avlitman <alitman@redhat.com>

---
+++ internal/httprouter/alerts_delete.go
@@ -0,0 +1,20 @@
+package httprouter
+
+import (
+	"net/http"
+)
+
+func (hr *httpRouter) DeleteUserDefinedAlertRuleById(w http.ResponseWriter, req *http.Request) {
+	ruleId, err := getParam(req, "ruleId")
+	if err != nil {
+		writeError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := hr.managementClient.DeleteUserDefinedAlertRuleById(req.Context(), ruleId); err != nil {
+		handleError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
